6c233142493d169e803066dbf85c292dfd960985
KekshinMaks <kekshin.maksim@gmail.com>
2026-04-18 22:16:01 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,44 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <array>
+
 template<typename T, int size>
-class TStack {
-  // добавьте код стека
+class Tstack {
+ private:
+  std::array<T, size> arr;
+  int top_idx;
+
+ public:
+  Tstack() : top_idx(-1) {}
+
+  void push(T value) {
+    if (top_idx < size - 1) {
+      arr[++top_idx] = value;
+    }
+  }
+
+  T pop() {
+    if (top_idx >= 0) {
+      return arr[top_idx--];
+    }
+    return T();
+  }
+
+  T get() const {
+    if (top_idx >= 0) {
+      return arr[top_idx];
+    }
+    return T();
+  }
+
+  bool isEmpty() const {
+    return top_idx == -1;
+  }
+
+  void clear() {
+    top_idx = -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
